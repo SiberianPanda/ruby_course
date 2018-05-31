ae14d4e0577237f42657f1dd5604a97b07bde4a1
SiberianPanda <max.bez.l@mail.ru>
2018-05-31 14:41:58 +0800

    Задача "НОД"

---
+++ task3.rb
@@ -0,0 +1,15 @@
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+def nod(mi,ma)
+    mod = ma%mi
+    if mod == 0
+        return mi
+    else
+        return nod(mod,mi)
+    end
+end
+if a>=b
+    puts(nod(ma = a,mi = b))
+else
+    puts(nod(ma = b,mi = a))
+end
