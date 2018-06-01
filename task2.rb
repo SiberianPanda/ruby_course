aa63f11fd052f6f7244414f3828b0ed5596bf29f
SiberianPanda <max.bez.l@mail.ru>
2018-06-01 11:47:33 +0800

    Перписана программа сдвига строки. Добавлены функции сдвига влево и вправо. Добавлена проверка на невозможность получения итоговой строки

--- task2.rb
+++ task2.rb
@@ -1,14 +1,39 @@
-a = ARGV[0].split('')
-b = ARGV[1].split('')
-a.each_index do |index_A|
-    if a[index_A] == b[0]
-        right = (a.length - index_A).abs
-        if index_A.abs <= right
-            puts(index_A)
-            exit()
+def right(a,b)
+    n_right = 0
+    flag = false
+    for i in (0..a.length)
+        if a == b
+            flag = true
+            break
         else
-            puts(right)
-            exit()
+            a.push(a.shift)
+            n_right += 1
         end
     end
+    return [n_right,flag]
+end
+def left(a,b)
+    n_left = 0
+    flag = false
+    for i in (0..a.length)
+        if a == b
+            flag = true
+            break
+        else
+            a.unshift(a.pop)
+            n_left += 1
+        end
+    end
+    return [n_left,flag]
+end
+n_right = right(Array.new(ARGV[0].split('')),Array.new(ARGV[1].split('')))
+n_left = left(Array.new(ARGV[0].split('')),Array.new(ARGV[1].split('')))
+if n_left[1] and n_right[1]
+    if n_left[0]<n_right[0]
+        puts(n_left[0])
+    else
+        puts(n_right[0])
+    end
+else
+    puts(-1)
 end
